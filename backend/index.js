90d42831b55e9e7fbe4e8de85ea8c34fc8df476a
Vansh Sharma <sharmavansh.3777@gmail.com>
2025-08-04 15:33:31 +0530

    commmit .env

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 const express =require('express')
 const rootRouter= require('./routes/index')
 const cors= require('cors')
-
+const PORT = process.env.PORT||5000
 const app=express()
+app.use(cors({
+  origin: 'https://quickpay-8.onrender.com',
+  credentials: true // optional, if using cookies or auth headers
+}));
+
 
 app.use(express.json())
 app.use(cors())
 app.use('/api/v1',rootRouter)
 
-app.listen(100,function(req,res){
-    console.log("server is running without intruption .. congratulations")
+app.listen(PORT ,function(req,res){
+    console.log("server is running without intruption .. ", PORT)
 })
