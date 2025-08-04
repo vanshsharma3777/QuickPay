e67d6607e811c67596eed5750fefe67f9a243b88
Vansh Sharma <sharmavansh.3777@gmail.com>
2025-08-04 16:33:08 +0530

    commit ast

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express =require('express')
 const rootRouter= require('./routes/index')
 const cors= require('cors')
-const PORT = process.env.PORT||5000
+const PORT = process.env.PORT || 2000
 const app=express()
 app.use(cors({
   origin: 'https://quickpay-8.onrender.com',
